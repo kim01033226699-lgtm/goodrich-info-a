6a472adb599e1d644c64e0aa1200d53288dc16e2
finok <kim01033226699@gmail.com>
2025-10-21 16:16:14 +0900

    푸터수정

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -249,7 +249,8 @@ function UserPage({ config }) {
       {/* Footer */}
       <footer className="footer">
         <div className="container">
-          <p>&copy; 2025 굿리치 지원금 안내. All rights reserved. (v1.0)</p>
+          <p>&copy; 2025 굿리치 영업지원 시스템. All rights reserved.</p>
+          <p style={{fontSize: '0.875rem', marginTop: '0.5rem'}}>본 페이지의 모든 정보는 내부 교육 자료이며, 무단 전재 및 배포를 금지합니다.</p>
         </div>
       </footer>
     </div>
