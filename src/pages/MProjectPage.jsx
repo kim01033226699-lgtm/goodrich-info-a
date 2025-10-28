9c35f7479500f3f39963ba4eccdd5654863c4f43
finok <kim01033226699@gmail.com>
2025-10-28 15:20:33 +0900

    새로고침아이콘수정

--- src/pages/MProjectPage.jsx
+++ src/pages/MProjectPage.jsx
@@ -404,11 +404,13 @@ function MProjectPage({ config }) {
                 </div>
               </div>
 
-              <button onClick={handleReset} className="btn-secondary btn-large btn-icon" title="새로고침">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                  <path d="M21.5 2v6h-6M2.5 22v-6h6M2 11.5a10 10 0 0 1 18.8-4.3M22 12.5a10 10 0 0 1-18.8 4.2"/>
-                </svg>
-              </button>
+              <div className="result-button-container">
+                <button onClick={handleReset} className="btn-secondary btn-icon" title="새로고침">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                    <path d="M21.5 2v6h-6M2.5 22v-6h6M2 11.5a10 10 0 0 1 18.8-4.3M22 12.5a10 10 0 0 1-18.8 4.2"/>
+                  </svg>
+                </button>
+              </div>
             </div>
           )}
         </div>
